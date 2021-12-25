4769ff4e846ff3cfbd3530b132baeedc16b80643
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 12:27:18 +0300

    Scripted auto-commit on change (2021-12-25 12:27:18) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab5/HttpServer.java
+++ src/main/java/ru/bmstu/akka/lab5/HttpServer.java
@@ -116,10 +116,7 @@ public class HttpServer {
                                 System.out.println("sending response from http client");
                                 CompletableFuture<Response> resp = client.prepareGet(url).execute().toCompletableFuture();
                                 return resp.
-                                        exceptionally( t -> {
-                                            return -1;
-                                        }).
-                                        thenApply(response -> {
+                                        thenCompose(response -> {
                                     long end = System.currentTimeMillis();
                                     System.out.println("received response from http client");
                                     return end - start;
