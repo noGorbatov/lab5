1dde7691b9640e21b866c869d5c0a3697af7c898
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 11:49:56 +0300

    Scripted auto-commit on change (2021-12-25 11:49:56) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab5/CacheActor.java
+++ src/main/java/ru/bmstu/akka/lab5/CacheActor.java
@@ -3,8 +3,32 @@ package ru.bmstu.akka.lab5;
 import akka.actor.AbstractActor;
 
 public class CacheActor extends AbstractActor {
+
+    public static class GetMsg {
+        private final String url;
+        private final int count;
+        public GetMsg(String url, int count) {
+            this.url = url;
+            this.count = count;
+        }
+        public int getCount() {
+            return count;
+        }
+        public String getUrl() {
+            return url;
+        }
+    }
+
+    public static class ResMsg {
+        private final boolean hasResult;
+        private final 
+    }
+
     @Override
     public Receive createReceive() {
-        return null;
+        receiveBuilder().
+                match(GetMsg.class, msg -> {
+                    getSender().tell();
+                })
     }
 }
