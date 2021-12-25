f25c25657fad5ffd775027847523908dfe6c8760
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 12:06:13 +0300

    Scripted auto-commit on change (2021-12-25 12:06:13) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab5/LoadTestApp.java
+++ src/main/java/ru/bmstu/akka/lab5/LoadTestApp.java
@@ -1,7 +1,11 @@
 package ru.bmstu.akka.lab5;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+
 public class LoadTestApp {
     public static void main(String[] args) {
-
+        ActorSystem system = ActorSystem.create();
+        final Http http
     }
 }
