2fe543d6e518f50688d1a6d9d188918ca78c34e5
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 12:06:56 +0300

    Scripted auto-commit on change (2021-12-25 12:06:56) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab5/LoadTestApp.java
+++ src/main/java/ru/bmstu/akka/lab5/LoadTestApp.java
@@ -2,20 +2,35 @@ package ru.bmstu.akka.lab5;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
 public class LoadTestApp {
-    public static void main(String[] args) {
+    final static private String HOST = "localhost";
+    final static private int PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create();
         final Http http = Http.get(system);
         final ActorMaterializer materializer =
                 ActorMaterializer.create(system);
         HttpServer server = new HttpServer(system, materializer);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createFlow();
-        final Completion
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer
+        );
+
+        System.in.read();
+
     }
 }
