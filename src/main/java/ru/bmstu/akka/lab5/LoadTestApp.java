a29c17a9653d342f3293b9c6cbea61a50a241d62
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 12:06:23 +0300

    Scripted auto-commit on change (2021-12-25 12:06:23) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab5/LoadTestApp.java
+++ src/main/java/ru/bmstu/akka/lab5/LoadTestApp.java
@@ -1,11 +1,21 @@
 package ru.bmstu.akka.lab5;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class LoadTestApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create();
-        final Http http
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+
     }
 }
