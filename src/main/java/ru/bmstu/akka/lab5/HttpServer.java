6ace04cba2c1a2793e9025c8218e7bfdc51e6d91
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 12:00:10 +0300

    Scripted auto-commit on change (2021-12-25 12:00:10) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab5/HttpServer.java
+++ src/main/java/ru/bmstu/akka/lab5/HttpServer.java
@@ -71,6 +71,8 @@ public class HttpServer {
                                         parsedRequest.getTestUrl(),
                                         res.getAverageTime()));
                     }
+
+                    
                 });
     }
 }
