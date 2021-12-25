03d598f0aa3de216e1159e28bd2c1d46f3bb2728
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 11:44:36 +0300

    Scripted auto-commit on change (2021-12-25 11:44:36) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab5/HttpServer.java
+++ src/main/java/ru/bmstu/akka/lab5/HttpServer.java
@@ -4,10 +4,16 @@ import akka.NotUsed;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.javadsl.Flow;
+import javafx.util.Pair;
 
 public class HttpServer {
 
     public Flow<HttpRequest, HttpResponse, NotUsed> createFlow() {
-        return Flow.of(HttpRequest.class)
+        return Flow.of(HttpRequest.class).
+                map( req -> )
+    }
+
+    private Flow<HttpRequest, Pair<String, Integer>> parseHttp(HttpRequest req) {
+        req
     }
 }
